c4339c5bf16175c38a91c70013859d038a05dd68
rozhan-rhi <rojanrahmani2002@yahoo.com>
2022-10-13 19:06:22 +0330

    code for saving to csv file added

--- myapp/data.js
+++ myapp/data.js
@@ -2,6 +2,8 @@ const express=require("express");
 const app=express();
 const fs=require("fs");
 const bodyParser=require("body-parser");
+const converter=require("json-2-csv");
+const filename="node.csv";
 app.use(bodyParser.urlencoded({extended:false}));
 app.set("views","./views");
 app.set("view engine","ejs");
@@ -9,9 +11,27 @@ app.get("/",function(req,res){
     res.render("sampleForm");
 });
 app.post("/saveData",(req,res)=>{
-    const body=req.body;
-    console.log(body);
-    res.send("successful!");
-
+    const reqbody=[req.body];
+    const csvStr=converter.json2csv(reqbody,(err,csv)=>{
+        if(fs.existsSync(filename)){
+            let value=[];
+            for(let item in req.body){
+                value.push(req.body[item])
+            }
+            let strValue=value.join(",")
+            fs.appendFile(filename,strValue,(err)=>{
+                if (err) throw err
+            })
+            }
+        else{
+            fs.writeFile(filename,csv,(err)=>{
+                if (err) throw err
+            })
+            res.send("successful!");
+        };
+    
+    });
+   
+  
 });
 app.listen(3000);
